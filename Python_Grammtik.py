4e9136297dbedeb7b72cad5df5e3710b58278bf7
gegner2019 <deguoliuxue2011@126.com>
2019-12-08 22:52:34 +0800

    add new

---
+++ Python_Grammtik.py
@@ -0,0 +1,173 @@
+import math
+
+print ('Hallo Team')
+
+print ('xiaojiu' + 'xiaozhu')
+
+print('xiaomai')
+
+print('lulu')
+
+
+print ('we will successed')
+
+
+print ('1111111111111')
+
+i=50
+
+j='i love you xiaojiu'
+
+m='now'
+
+n='forever'
+
+print(j+' '+m)
+
+if m=='now':
+    print('you are right')
+else:
+    print('you are wrong')
+item_one=1
+item_two=2
+
+total1=item_one+item_two+item_two
+
+total2 = item_one + \
+        item_two + \
+        item_two
+
+print(total1)
+
+print(total2)
+
+num= 123456789
+num1=1
+num2=2
+num3=num1+num2
+print(num3)
+str1='1'
+str2='2'
+str3=str1+str2
+print(str3)
+
+str='1234567891231231255555555555511111111111111111111111111111111111111111111111111111111111111111111111111111'  + \
+     '12341254125421561514123123'
+str1='5512312312451231'
+
+print(str+'\n'+str1)
+
+print(str, end="aaaaaaaaaaaaaaaaaaaaaaaaaa")
+print(str1)
+
+print(str+str1)
+
+
+m=29
+if m%2==1:
+    print('i am single')
+
+
+a=1
+a+=3
+
+a+=10 #a=a+10
+
+a-=5
+
+a/=2
+
+a%=2 #a=a%2
+
+a=1 #a=0000 0001
+
+b=60 #b= 0011 1100
+
+a=60 #a=0011 1100
+#a=0111 1000
+
+print(a*2)
+
+print(a<<1)
+
+x=10
+
+
+
+y=10
+
+if(x and y ):
+    print('all True')
+else:
+    print('one or all false')
+
+
+y=100
+
+if (x is y):
+    print('x is y')
+else:
+    print('x is not y')
+
+x=10
+
+print(x)
+
+x=float(x)
+
+
+x=1.125456
+
+print(round(x,2))
+
+print(max(1,2,3,4,5,5,6,7))
+
+str="you are shit"
+
+print('1',str[0:2])
+
+print('[0]',str[0:2])
+
+
+
+print(str[0:2])
+
+
+str1='987654321'
+
+print(str1[6:7])
+
+list0=[1,23,4,5,65]
+
+list1=['1',2,'kljlkajsdlkj', list0]
+
+print(list0[1])
+
+print(list1[3])
+
+list0[2]=10000
+
+#print(list0)
+
+del list0[2]
+
+#print(list0)
+
+#list0.append(1)
+
+#list0.clear()
+
+list1=[1,2,3,4,5,6,7]
+
+list1.pop(2)
+
+list1.remove('1')
+
+
+
+print(list1)
+
+
+
+
+
